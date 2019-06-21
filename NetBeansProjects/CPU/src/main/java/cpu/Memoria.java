9e87a33ad330c1bc004f2e313df9f1267668bed5
Raul <Raul@Raul-PC>
2019-06-20 21:12:21 -0300

    Commit Memoria

--- NetBeansProjects/CPU/src/main/java/cpu/Memoria.java
+++ NetBeansProjects/CPU/src/main/java/cpu/Memoria.java
@@ -1,6 +1,14 @@
 package cpu;
 import java.util.*;
 public class Memoria {
-     
-     LinkedList<String> ListaComando = new LinkedList<String>();
+  LinkedList<String> ListaComando = new LinkedList<String>();
+  
+//Inserir na memoria usando a Linha da memoria e o Upcode da operaçao
+  public void InserirMemoria(int indice,String code){
+   ListaComando.add(indice,code); 
+  }   
+//Retorna o Upcode que ta na memoria de acordo com o Indice
+ public String RetornarMemoria(int indice){
+  return ListaComando.get(indice); 
+ }
 }
